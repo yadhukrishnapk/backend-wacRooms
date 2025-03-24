76b6a0d06d08ef1b5c76f9c3a2ee1f0ff7d3279b
yadhukrishnapk <yadhukrishna.pk@webandcrafts.in>
2025-03-24 14:33:02 +0530

    Update index

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(',')
-  : ["http://localhost:5173", "http://localhost:5174"];
+  : ["http://localhost:5173", "http://localhost:5174","https://frontend-wac-rooms.vercel.app"];
 
 app.use(cors({
   origin: function (origin, callback) {
